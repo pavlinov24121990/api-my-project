0b62a5a510d06a0df2fa95fe0e8c902613355bfb
Aleksey <pavlinov24121990@gmail.com>
2023-11-30 17:59:25 +0100

    finish1

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -3,7 +3,6 @@
 module Api
   module V1
     class ProductsController < ApplicationController
-      before_action :current_user!
       def index
         pagy, products = pagy(Product.all, items: 6)
         render json: {
